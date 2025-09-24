473d22edd818e56553c095c3f9bf2bf910cc5e30
mudassir aliyu <mudassiraliyuwsh@gmail.com>
2025-09-24 20:01:28 +0100

    solved new loop 8kyu challenge

--- 8Kyu/app.js
+++ 8Kyu/app.js
@@ -79,3 +79,14 @@ function checkWon(player1, player2){
     return "Draw!!"
  }
 }
+
+/*Oh no, Timmy's created an infinite loop!
+ Help Timmy find and fix the bug in his unfinished for loop!*/
+
+ function createArray(number) {
+    const newArray = [];
+    for (let counter = 1; counter <= number; counter++) {
+        newArray.push(counter);
+    }
+    return newArray;
+}
